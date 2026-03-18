07bf14c6dbdf77208f243a931983d4df94602e6c
Alexander Anderson <andersonWork306@gmail.com>
2026-03-17 19:09:09 -0700

    Initial Commit

---
+++ src/perlinNoise.zig
@@ -0,0 +1,113 @@
+const rl = @import("raylib");
+const std = @import("std");
+const random = std.Random;
+
+fn randomGradient(rng: anytype) rl.Vector2 {
+    const theta = rng.float(f32) * 2.0 * std.math.pi;
+    return .{
+        .x = @cos(theta),
+        .y = @sin(theta),
+    };
+}
+
+fn lerp(a: f32, b: f32, t: f32) f32 {
+    //linear interpilation math
+    return a + t * (b - a);
+}
+
+fn fade(t: f32) f32 {
+    //magic fade math
+    return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
+}
+
+pub fn generatePerlinNoise(allocator: std.mem.Allocator, height: u32, width: u32, squaresPerLat: u8, seed: u64) ![][]f32 {
+    const errors = error{ InvalidHeight, InvalidWidth, InvalidSquaresPerLat };
+    if (height > rl.getMonitorHeight(rl.getCurrentMonitor())) {
+        return errors.InvalidHeight;
+    }
+
+    if (height == 0) {
+        return errors.InvalidHeight;
+    }
+
+    if (width > rl.getMonitorWidth(rl.getCurrentMonitor())) {
+        return errors.InvalidWidth;
+    }
+
+    if (width == 0) {
+        return errors.InvalidWidth;
+    }
+
+    if (squaresPerLat == 0) {
+        return errors.InvalidSquaresPerLat;
+    }
+
+    const spl: u32 = @intCast(squaresPerLat);
+    //lattice height and width is the celing devision of screen squares/squares per lattice
+    //this covers the case where we actually need an extra lattice
+    const latticeHeight = (height + spl - 1) / spl;
+    const latticeWidth = (width + spl - 1) / spl;
+
+    //this is the lattice for our squares
+    var latticeVectors = try allocator.alloc([]rl.Vector2, latticeHeight + 1);
+
+    defer {
+        for (latticeVectors) |row| {
+            allocator.free(row);
+        }
+
+        allocator.free(latticeVectors);
+    }
+
+    var prng = random.DefaultPrng.init(seed);
+    const rng = prng.random();
+
+    //initialize the random gradients
+    for (0..latticeHeight + 1) |i| {
+        latticeVectors[i] = try allocator.alloc(rl.Vector2, latticeWidth + 1);
+
+        for (0..latticeWidth + 1) |j| {
+            latticeVectors[i][j] = randomGradient(rng);
+        }
+    }
+
+    var squareNoise = try allocator.alloc([]f32, height);
+    //big generate perlin noise step
+    for (0..height) |i| {
+        squareNoise[i] = try allocator.alloc(f32, width);
+        for (0..width) |j| {
+            //calculate the current lattice square we are in
+            const latticeI = i / spl;
+            const latticeJ = j / spl;
+
+            //calculate the squares print position within the lattice square
+            const localI = i % spl;
+            const localJ = j % spl;
+
+            //our squares are unit squares. So the center of one square is sqrt(0.5 * 0.5)
+            const fy = (@as(f32, @floatFromInt(localI)) + 0.5) / @as(f32, @floatFromInt(spl));
+            const fx = (@as(f32, @floatFromInt(localJ)) + 0.5) / @as(f32, @floatFromInt(spl));
+
+            const blVect = rl.Vector2.init(fx, fy);
+            const brVect = rl.Vector2.init(fx - 1.0, fy);
+            const tlVect = rl.Vector2.init(fx, fy - 1.0);
+            const trVect = rl.Vector2.init(fx - 1.0, fy - 1.0);
+
+            //get the dot product of the gradients and the distances
+            const dotbl = latticeVectors[latticeI][latticeJ].dotProduct(blVect);
+            const dotbr = latticeVectors[latticeI][latticeJ + 1].dotProduct(brVect);
+            const dottl = latticeVectors[latticeI + 1][latticeJ].dotProduct(tlVect);
+            const dottr = latticeVectors[latticeI + 1][latticeJ + 1].dotProduct(trVect);
+
+            const u = fade(fx);
+            const v = fade(fy);
+            const ix0 = lerp(dotbl, dotbr, u);
+            const ix1 = lerp(dottl, dottr, u);
+            const value = lerp(ix0, ix1, v);
+
+            squareNoise[i][j] = value;
+        }
+    }
+
+    return squareNoise;
+}
